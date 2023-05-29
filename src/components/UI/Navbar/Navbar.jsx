88fd19b4d9aeb7fa150d7a89a07c6b80036148a7
Евгений <fandeev012@gmail.com>
2023-05-29 08:32:54 +0300

    Изменены стили Navbar

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -15,8 +15,8 @@ const Navbar = () => {
         <div className="navbar">
             <MyButton onClick={logout}>Выйти</MyButton>
             <div className="navbar__links">
-                <Link to="/about">О сайте</Link >
-                <Link to="/posts">Посты</Link >
+                <Link to="/about" className="navbar__link">О сайте</Link >
+                <Link to="/posts" className="navbar__link">Посты</Link >
             </div>
         </div>
     );
